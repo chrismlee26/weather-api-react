931be41bc99504b64fb138ada9c59e0daa2c3483
CHRIS_LEE <chrismlee87@gmail.com>
2021-06-28 17:55:04 -0700

    weather and radio components

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,54 @@
+import { truncate } from 'lodash'
 import { useState } from 'react'
 import './Weather.css'
 
+import RadioButton from './RadioButton'
+
 function Weather() {
+  const [zip, setZip] = useState('94103')
+  const [unit, setUnit] = useState('')
+
   return (
     <div className="Weather">
+      <h1>{zip} {unit}</h1>
       <form>
-        <input
-          placeholder="Enter Zip Code"
+        <div>
+          <input
+            placeholder="Enter Zip Code"
+            value={zip}
+            onChange={e => setZip(e.target.value)}
+          />
+          <button>Submit</button>
+        </div>
+        <select
+          onChange={e => setUnit(e.target.value)}
+          value={unit}
+        >
+          <option value="metric">Celsius</option>
+          <option value="imperial">Fahrenheit</option>
+          <option value="standard">Kelvin</option>
+        </select>
+
+        <RadioButton
+          label="metric"
+          name="unit"
+          checked={unit === 'metric'}
+          onChange={() => setUnit('metric')}
         />
-        <button>Submit</button>
+
+        <RadioButton
+          label="imperial"
+          name="unit"
+          checked={unit === 'imperial'}
+          onChange={() => setUnit('imperial')}
+        />
+
+        <RadioButton
+          label="standard"
+          name="unit"
+          checked={unit === 'standard'}
+          onChange={() => setUnit('standard')}
+        />
 
       </form>
     </div>
