70b345e15315b80d13294f81cfdc76b75274667a
CHRIS_LEE <chrismlee87@gmail.com>
2021-07-01 13:12:56 -0700

    added geo location, env

--- src/Weather.js
+++ src/Weather.js
@@ -3,15 +3,83 @@ import { useState } from 'react'
 import './Weather.css'
 
 import RadioButton from './RadioButton'
+import WeatherDisplay from './WeatherDisplay'
 
 function Weather() {
   const [zip, setZip] = useState('94103')
   const [unit, setUnit] = useState('')
+  const [data, setData] = useState(null)
+
+  // -------------------------------
+
+  function fetchWeatherByGeo() {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0    
+    }
+
+    // get geo coords
+    navigator.geolocation.getCurrentPosition(pos => {
+      const { latitude, longitude } = pos.coords
+      const apiKey = process.env.REACT_APP_API_KEY;
+      const path = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${unit}`;
+      fetchWeather(path)
+    }, err => {
+      console.log(err.message)
+    }, options)
+    
+
+  }
+
+  function fetchWeatherByZip() {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${unit}`;
+    fetchWeather(path)
+  }
+
+  async function fetchWeather(path) {
+    // convert to json
+    const res = await fetch(path)
+    const json = await res.json()
+    // console.log(json)
+
+    //  error handling
+    const cod = json.cod
+    const message = json.message
+
+    if (cod !== 200) {
+      setData({ cod, message })
+      return
+    }
+
+    // success properties
+    const temp = json.main.temp
+    const feelsLike = json.main.feels_like
+    const description = json.weather[0].description
+
+    // setData
+    setData({
+      cod,
+      message,
+      temp,
+      feelsLike,
+      description
+    })
+  }
+  // -------------------------------
 
   return (
     <div className="Weather">
+      {/* {data && <WeatherDisplay {...data} />} */}
+      {data ? <WeatherDisplay {...data} /> : <p>Ready</p>}
       <h1>{zip} {unit}</h1>
-      <form>
+
+      <form onSubmit={e => {
+        e.preventDefault()
+        fetchWeatherByZip()
+      }}>
+
         <div>
           <input
             placeholder="Enter Zip Code"
@@ -51,6 +119,9 @@ function Weather() {
         />
 
       </form>
+      <button
+        onClick={() => fetchWeatherByGeo()}
+      >Get weather by location</button>
     </div>
   )
 }
